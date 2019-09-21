5a45e1b7ba9883959c09272e1d5d3f461380cddc
Laura Berge <lauraeberge@gmail.com>
2019-09-21 22:34:26 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -18,7 +18,7 @@ class Pokemon
       WHERE id = ?
       SQL
     row = db.execute(sql, id)
-    Pokemon.new(row[0], row[1], row[2], row[3])
+    Pokemon.new(id: row[0], name: row[1], type: row[2], db: db)
   end
   
 end
