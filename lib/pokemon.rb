058baa7427e2969f7ac7d19b921443a28595b521
Laura Berge <lauraeberge@gmail.com>
2019-09-21 22:03:52 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,8 +1,13 @@
 class Pokemon
   attr_accessor :name, :type, :db, :id
   
-  def initialize(name:, type:, db:, id:)
+  def initialize(id:, name:, type:, db:)
     
+  end
+  
+  def self.save(name, type, db)
+    id = DB[:conn].execute("")
+    pokemon = Pokemon.new(name, type, db)
   end
   
 end
