5bec5809e970083b433c09ccc964c8b9850a4e3b
Laura Berge <lauraeberge@gmail.com>
2019-09-21 22:22:07 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -9,7 +9,7 @@ class Pokemon
   end
   
   def self.save(name, type, db)
-    sql = 
+    db.execute("INSERT INTO ")
     
     #id = @db.execute("SELECT last_insert_rowid() FROM students")[0][0]
     #pokemon = Pokemon.new(id, name, type, db)
